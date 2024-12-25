883e68a1b6cde44e31580330c79894985093adcb
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-12-25 23:47:49 +0900

    Rename stuffs

--- index.js
+++ index.js
@@ -32,23 +32,24 @@ const NOTCHES = [
   { delta: 9.5, length: 1.4 },
 ];
 
-const outer_circle_svg = `<g id="outer"><circle cx="0" cy="0" r="74" stroke="black" stroke-width=".25" fill="white" id="circle2" style="fill:#eee;" />`
+const WINDOW_RADIUS = { exterior: 39, interior: 26 };
+
+const outer_plate_svg = `<g id="outer"><circle cx="0" cy="0" r="74" stroke="black" stroke-width=".25" fill="white" id="circle2" style="fill:#eee;" />`
   + FREQS_BY_10.flatMap(
-    (freq) => NOTCHES.map(o => notch_on_outer_circle(o.length, freqToAngle(freq + o.delta)))
+    (freq) => NOTCHES.map(o => notch_on_outer_plate(o.length, freqToAngle(freq + o.delta)))
   ).join('')
-  + FREQS_BY_10.map((freq) => text_on_outer_circle(freq, freqToAngle(freq))).join('') + `</g>`;
+  + FREQS_BY_10.map((freq) => text_on_outer_plate(freq, freqToAngle(freq))).join('') + `</g>`;
 
 
-const inner_circle_svg = () => {
+const inner_plate_svg = () => {
 
-  const o = { exterior: 39, interior: 26 };
-  const window = `M 0,-${o.interior} L 0,-${o.exterior} A ${o.exterior} ${o.exterior} 0 0 0 -${o.exterior * Math.SQRT1_2},-${o.exterior * Math.SQRT1_2} L -${o.interior * Math.SQRT1_2},-${o.interior * Math.SQRT1_2} A ${o.interior} ${o.interior} 0 0 1 0,-${o.interior}`;
+  const window = `M 0,-${WINDOW_RADIUS.interior} L 0,-${WINDOW_RADIUS.exterior} A ${WINDOW_RADIUS.exterior} ${WINDOW_RADIUS.exterior} 0 0 0 -${WINDOW_RADIUS.exterior * Math.SQRT1_2},-${WINDOW_RADIUS.exterior * Math.SQRT1_2} L -${WINDOW_RADIUS.interior * Math.SQRT1_2},-${WINDOW_RADIUS.interior * Math.SQRT1_2} A ${WINDOW_RADIUS.interior} ${WINDOW_RADIUS.interior} 0 0 1 0,-${WINDOW_RADIUS.interior}`;
 
   return `<g id="inner"><path fill="white" fill-rule="evenodd" d="M 0,-60 A 60 60 0 0 1 0,60 A 60 60 0 0 1 0,-60 ${window}" transform="rotate(22.5)" />`
     + FREQS_BY_10.flatMap(
-      (freq) => NOTCHES.map(o => notch_on_inner_circle(o.length, freqToAngle(freq + o.delta)))
+      (freq) => NOTCHES.map(o => notch_on_inner_plate(o.length, freqToAngle(freq + o.delta)))
     ).join('')
-    + FREQS_BY_10.map((freq) => text_on_inner_circle(freq, freqToAngle(freq))).join('')
+    + FREQS_BY_10.map((freq) => text_on_inner_plate(freq, freqToAngle(freq))).join('')
     + Array.from({ length: 12 }, (_, i) => text({
       radius: 45, angle: i * 30, content: [
         'A', 'A♯/B♭', 'B', 'C', 'C♯/D♭', 'D',
@@ -62,8 +63,8 @@ const inner_circle_svg = () => {
 
 fs.writeFileSync('gen.svg', `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <svg version="1.1" viewBox="-80 -80 160 160">`+
-  outer_circle_svg
-  + inner_circle_svg()
+  outer_plate_svg
+  + inner_plate_svg()
   + `</svg>
 `, { encoding: 'utf-8' });
 
@@ -83,15 +84,15 @@ function text({ content, angle, radius }) {
        style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:4px;font-family:Garamond;-inkscape-font-specification:Garamond;text-align:center;text-anchor:middle">${content}</tspan></text>`;
 }
 
-function text_on_inner_circle(content, angle) {
+function text_on_inner_plate(content, angle) {
   return text({ content, angle, radius: 52 });
 }
 
-function text_on_outer_circle(content, angle) {
+function text_on_outer_plate(content, angle) {
   return text({ content, angle, radius: 65 });
 }
 
-function notch_on_inner_circle(length, angle) {
+function notch_on_inner_plate(length, angle) {
   return `<path
      style="fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:0.2;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none" transform="rotate(${angle})"
      d="m 0,-60 v ${length}"
@@ -99,7 +100,7 @@ function notch_on_inner_circle(length, angle) {
      sodipodi:nodetypes="cc" />`;
 }
 
-function notch_on_outer_circle(length, angle) {
+function notch_on_outer_plate(length, angle) {
   return `<path
      style="fill:#ffffff;fill-opacity:0.5;stroke:#000000;stroke-width:0.2;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none" transform="rotate(${angle})"
      d="m 0,-60 v ${-length}"
